e645ea2423a731776d509baec9b499086d16c5ff
RobinSaldo179 <153888294+RobinSaldo179@users.noreply.github.com>
2024-10-12 19:14:33 +0800

    Add files via upload

--- static/js/base.js
+++ static/js/base.js
@@ -1,1 +1,18 @@
-console.log("Javascript here!")
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
